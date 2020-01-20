f6b4db20e0d9840547c329ce8cf4eaca5d2050d7
Tulasi G <Tulasi.Gudibanda@ibm.com>
2020-01-20 14:43:30 -0600

    feat(tutorial): complete step 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import './app.scss';
+// import './app.scss';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
 import RepoPage from './content/RepoPage';
+
+import './App.scss';
+
+// import { Button } from 'carbon-components-react';
 
 class App extends Component {
   render() {
